2b56d527d2ed459123dfc4b2e2eec5bb0f97d723
kj.kang <miso1459@naver.com>
2025-06-08 12:28:10 +0900

    홈 화면 초안안

--- myDemo/src/routes/+page.svelte
+++ myDemo/src/routes/+page.svelte
@@ -1,15 +1,17 @@
-<script>
-    import Page_Query from '$lib/Page_Query.svelte';
-    import { minMaxFilterEditor, minMaxFilterFunction } from '$lib/Tabulator/minMaxFilterEditor.js';
-
-    const columns = [
-        { title: "ID", field: "id", maxWidth:100, headerFilter:"number", headerFilterPlaceholder:"at least...", headerFilterFunc:">="},
-        { title: "Name", field: "name", minWidth:100, maxWidth:200, headerFilter:true, headerFilterPlaceholder:"Find a Person...", headerFilterFunc:"like", headerFilterLiveFilter:true},
-        { title: "Age", field: "age", maxWidth:100, headerFilter:minMaxFilterEditor, headerFilterFunc:minMaxFilterFunction, headerFilterLiveFilter:false},
-        { title: "Gender", field: "gender", maxWidth:100, editor:"list", editorParams:{values:{"male":"Male", "female":"Female", clearable:true}}, headerFilter:true, headerFilterPlaceholder:"Select...", headerFilterParams:{values:{"male":"Male", "female":"Female", "":""}, clearable:true}, readonly:true},
-        { title: "Date", field: "date", maxWidth:100, headerFilter:true, clearable:true}
-    ];
+<script lang="ts">
+  import IconBookmark from '@lucide/svelte/icons/bookmark';
+  const diagramCircle = 'preset-tonal w-8 aspect-square flex justify-center items-center rounded-full';
 </script>
 
-<Page_Query {columns} api="/api/users" />
-
+<div>
+    <button type="button" class="btn preset-tonal-primary" 
+        style="width: calc(100vw - 20px);; justify-content: flex-start; margin: 3px 10px 2px 10px;">
+        Tonal
+    </button>
+    
+    <a href="/T07.Skeleton" style="display: block; width: calc(100vw - 40px); margin: 3px 10px 2px 30px;">
+        <button type="button" class="btn preset-outlined-primary-500" style="width: 100%; justify-content: flex-start;">
+            T07.Skeleton 이동
+        </button>
+    </a>
+</div>
