b97229dc0b3446e3f13b1653fff8df09eeb49722
kj.kang <miso1459@naver.com>
2025-06-08 12:38:50 +0900

    홈화면 2

--- myDemo/src/routes/+page.svelte
+++ myDemo/src/routes/+page.svelte
@@ -3,13 +3,20 @@
   const diagramCircle = 'preset-tonal w-8 aspect-square flex justify-center items-center rounded-full';
 </script>
 
+  <div class="preset-tonal-success flex items-center justify-center p-4"
+    style="width: calc(100vw - 10px); margin: 3px 5px 2px 5px;">
+  
+    <span class="text-2xl font-bold">UNIERP - EMERP</span>
+    <span class="text-xs ml-5">사용자 ID</span>
+  </div>
+
 <div>
     <button type="button" class="btn preset-tonal-primary" 
-        style="width: calc(100vw - 20px);; justify-content: flex-start; margin: 3px 10px 2px 10px;">
+        style="width: calc(100vw - 10px);; justify-content: flex-start; margin: 3px 5px 2px 5px;">
         Tonal
     </button>
-    
-    <a href="/T07.Skeleton" style="display: block; width: calc(100vw - 40px); margin: 3px 10px 2px 30px;">
+
+    <a href="/T07.Skeleton" style="display: block; width: calc(100vw - 30px); margin: 3px 5px 2px 25px;">
         <button type="button" class="btn preset-outlined-primary-500" style="width: 100%; justify-content: flex-start;">
             T07.Skeleton 이동
         </button>
